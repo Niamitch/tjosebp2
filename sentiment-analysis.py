c3fc1aa64a5754ec389287ffe445dc6e5da9911b
jo <kara1275@hotmail.com>
2018-10-26 16:19:08 -0700

    changed encoding

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -3,7 +3,7 @@ import re
 import nltk
 import os
 import random
-
+import numpy as np
 
 PROVERBE_LANGUAGE = "french"
 grammes = {}
@@ -21,16 +21,16 @@ def get_sentiment_features(line):
 
 def main():
     for i in os.listdir("./books/Book/neg_Bk"):
-        with open("./books/Book/neg_Bk/"+i) as f:
+        with open("./books/Book/neg_Bk/"+i, encoding = "ISO-8859-1") as f:
             features = get_sentiment_features(f.read())
             featuresSet.append((features,0))
     for i in os.listdir("./books/Book/pos_Bk"):
-        with open("./books/Book/pos_Bk/"+i) as f:
+        with open("./books/Book/pos_Bk/"+i, encoding = "ISO-8859-1") as f:
             features = get_sentiment_features(f.read())
             featuresSet.append((features, 1))
     random.shuffle(featuresSet)
     size = len(featuresSet)
-    train_set, test_set = featuresSet[size*0.8:], featuresSet[:size*0.2]
+    train_set, test_set = featuresSet[int(size*0.8):], featuresSet[:int(size*0.2)]
     classifier = nltk.NaiveBayesClassifier.train(train_set)
     print(nltk.classify.accuracy(classifier, test_set))
 
