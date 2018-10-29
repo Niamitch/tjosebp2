6cb4af819c6a2b8f7e11f02502ab15b8ee7f0b60
jo <kara1275@hotmail.com>
2018-10-28 20:37:19 -0700

    beginning language identification

--- language-identification.py
+++ language-identification.py
@@ -5,8 +5,8 @@ import random
 # bigramme beaucoup plus stable que trigramme pour une raison obscure
 def create_gramme(sentence):
     features = {}
-    for i in range(len(sentence)-1):
-        gramme = sentence[i:i+2]
+    for i in range(len(sentence)-2):
+        gramme = sentence[i:i+3]
         if gramme not in features:
             features[gramme] = 1
         else:
@@ -33,8 +33,11 @@ def main():
     random.shuffle(features_set)
     size = len(features_set)
     train_set, test_set = features_set[:int(size * 0.8)], features_set[int(size * 0.8):]
-    classifier = nltk.NaiveBayesClassifier.train(train_set)
-    print(nltk.classify.accuracy(classifier, test_set))
+    bayesClassifier = nltk.NaiveBayesClassifier.train(train_set)
+    algorithm = nltk.classify.MaxentClassifier.ALGORITHMS[0]
+    regClassifier = nltk.MaxentClassifier.train(train_set, algorithm, max_iter=3)
+    print(nltk.classify.accuracy(bayesClassifier, test_set))
+    print(nltk.classify.accuracy(regClassifier, test_set))
 
 if __name__ == "__main__":
    main()
